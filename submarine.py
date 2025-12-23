463faac9916d47e61937a68ab5c88f77ea8d4779
student <ty@gmail.com>
2025-12-23 21:21:13 +0300

    add image for submarine

--- submarine.py
+++ submarine.py
@@ -1,13 +1,18 @@
 import tkinter
-
+import os 
+from PIL import ImageTk, Image
 
 class Submarine:
     canvas: tkinter.Canvas
     image : int
+    __image_path: str = os.path.join(os.getcwd(), 'submarine.png')
 
     def __init__(self, canvas):
         self.canvas = canvas 
-        self.image = self.canvas.create_oval(200, 200, 240, 240, fill='yellow')
+        image_file = Image.open(self.__image_path).resize((120,100), resample= Image.Resampling.LANCZOS)
+
+        self.__image_src= ImageTk.PhotoImage(image_file)
+        self.image = self.canvas.create_image(200,200, image=self.__image_src)
 
     def __del__(self):
         self.canvas.delete(self.image)
