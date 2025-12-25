ebaa0a7e447e4c15ef4875289b930df63c75b52b
student <ty@gmail.com>
2025-12-25 20:53:36 +0300

    mirror submarine

--- bubble.py
+++ bubble.py
@@ -21,4 +21,14 @@ class Bubble:
     
     def move(self):
         self.canvas.move(self.image,randint(-2,2), -self.speed)
+        x1,y1,x2,y2 = self.canvas.coords(self.image)
+        if y2 < 0:
+            window_height = self.canvas.winfo_height()
+            self.canvas.move(self.image,0,window_height + randint(10,20) )
+        
+        window_width= self.canvas.winfo_width()
+        if x1 > window_width:
+            self.canvas.move(self.image,-self.r,0)
+        if x2 < 0:
+            self.canvas.move(self.image,self.r,0)
 
--- submarine.py
+++ submarine.py
@@ -2,6 +2,11 @@ import tkinter
 import os 
 from PIL import ImageTk, Image
 
+
+
+
+
+ 
 class Submarine:
     canvas: tkinter.Canvas
     image : int
@@ -10,20 +15,25 @@ class Submarine:
     def __init__(self, canvas):
         self.canvas = canvas 
         image_file = Image.open(self.__image_path).resize((120,100), resample= Image.Resampling.LANCZOS)
-
-        self.__image_src= ImageTk.PhotoImage(image_file)
-        self.image = self.canvas.create_image(200,200, image=self.__image_src)
+        self.__image_src_right= ImageTk.PhotoImage(image_file.transpose(Image.FLIP_LEFT_RIGHT))
+        self.__image_src_left= ImageTk.PhotoImage(image_file)
+        self.image = self.canvas.create_image(200,200, image=self.__image_src_right)
+        # self.image = self.canvas.create_image(200,200, image=self.submarine_left)
+        self.direction = "left" 
 
     def __del__(self):
         self.canvas.delete(self.image)
 
     
     def move(self,event):
-        if event.keysym == "Up":
-            self.canvas.move(self.image, 0, -5)
-        if event.keysym == "Down":
-            self.canvas.move(self.image, 0, 5)
-        if event.keysym == "Left":
-            self.canvas.move(self.image, -5, 0)
-        if event.keysym == "Right":
-            self.canvas.move(self.image, 5, 0)
+
+            if event.keysym == "Up":
+                self.canvas.move(self.image, 0, -5)
+            if event.keysym == "Down":
+                self.canvas.move(self.image, 0, 5)
+            if event.keysym == "Left":
+                self.canvas.move(self.image, -5, 0)
+                self.canvas.itemconfig(self.image, image = self.__image_src_left)
+            if event.keysym == "Right":
+                self.canvas.move(self.image, 5, 0)
+                self.canvas.itemconfig(self.image, image = self.__image_src_right)
